411d97029a04478719fe232010e313c7c3c33f38
Parth-88 <112095322+Parth-88@users.noreply.github.com>
2023-09-26 15:38:17 +0530

    Create Parth_2(moveZero).cpp

---
+++ Parth_2(moveZero).cpp
@@ -0,0 +1,20 @@
+lass Solution {
+public:
+    void moveZeroes(vector<int>& nums)
+    {
+    int last = 0, cur = 0;
+    
+    while(cur < nums.size()) 
+    {
+        if(nums[cur] != 0) 
+        {
+            swap(nums[last], nums[cur]);
+            last++;
+        }
+        
+        cur++;
+    }
+}
+        
+    
+};
