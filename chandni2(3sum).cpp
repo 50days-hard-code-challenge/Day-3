3f27dc510700af6ba813673ea471e31e59e1a8d3
Chandni Gupta <125583314+nainagupta4571@users.noreply.github.com>
2023-09-27 00:04:51 +0530

    Create chandni2(3sum).cpp

---
+++ chandni2(3sum).cpp
@@ -0,0 +1,42 @@
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    vector<int> v = {nums[i], nums[j], nums[k]};
+                    res.push_back(v);
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+
+                    j++;
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+};
+
